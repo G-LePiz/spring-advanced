1710ab3a2964d6aadf9602229403e1cf366bc0d8
dawon <aodlstory321@gmail.com>
2025-02-24 10:16:05 +0900

    chore:LV.1 Validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,9 @@ import lombok.NoArgsConstructor;
 public class UserChangePasswordRequest {
 
     @NotBlank
+
     private String oldPassword;
     @NotBlank
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$") // 대문자 포함, 숫자를 넣고, 8자리 이상인 비밀번호 사용
     private String newPassword;
 }
