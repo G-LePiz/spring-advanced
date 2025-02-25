1662d1225156946c14195c0f38492d9062cef13c
dawon <aodlstory321@gmail.com>
2025-02-25 16:16:57 +0900

    chore:Application Propertity 추가

---
+++ src/main/java/org/example/expert/config/interceptor/AuthInterceptor.java
@@ -0,0 +1,14 @@
+package org.example.expert.config.interceptor;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class AuthInterceptor implements HandlerInterceptor {
+
+//    @Override
+//    public boolean preHanddle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//
+//    }
+}
